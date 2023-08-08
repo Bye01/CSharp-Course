cc1346e697f6584cf0e692175aab13a3dd114abc
B y e <140664485+Bye01@users.noreply.github.com>
2023-08-08 20:25:09 -0300

    Objetos_e_Projetos

--- ConsoleApp3/ConsoleApp3/obj/Debug/net7.0/Objetos e Projetos.AssemblyInfo.cs
+++ ConsoleApp3/ConsoleApp3/obj/Debug/net7.0/Objetos e Projetos.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp4")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Objetos e Projetos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp4")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp4")]
+[assembly: System.Reflection.AssemblyProductAttribute("Objetos e Projetos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Objetos e Projetos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
