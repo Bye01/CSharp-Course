9e8dc79d4a809162db4bf81a6f157d1593859e5a
B y e <140664485+Bye01@users.noreply.github.com>
2023-08-09 19:48:35 -0300

    Estrutura Sequencial ex1

---
+++ Estrutura Sequencial/Estrutura Sequencial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estrutura_Sequencial
+{
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        ex1();
+    }
+    static void ex1()
+    {
+
+        int A, B, soma;
+
+        Console.WriteLine("coloque dois numeros");
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        soma = A + B;
+
+        Console.WriteLine("SOMA = " + soma);
+        //  Console.WriteLine("SOMA = " + A + B);
