bf192c2a593aa9521def840c694a72732ce86cb6
B y e <140664485+Bye01@users.noreply.github.com>
2023-08-09 21:37:15 -0300

    Estrutura Sequencial ex2

---
+++ Estrutura Sequencial ex2/Estrutura Sequencial ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Estrutura_Sequencial_ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
